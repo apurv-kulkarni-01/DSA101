141d833965756ad854f0aa8cfa73576536cbdf4c
Apurv <apurvkulkarni29@gmail.com>
2022-06-14 23:56:22 +0530

    searching idx_first_occurence

--- Searching/binary_recursive.cpp
+++ Searching/binary_recursive.cpp
@@ -11,11 +11,11 @@ int binary_recursive(int arr[], int low, int high, int x)
         return mid;
     else if(arr[mid]>x)
     {
-        binary_recursive(arr,low, mid-1, x);
+        return binary_recursive(arr,low, mid-1, x);
     }
     else if(arr[mid]<x)
     {
-        binary_recursive(arr, mid+1, high, x);
+        return binary_recursive(arr, mid+1, high, x);
     }
 
 }
@@ -24,7 +24,7 @@ int binary_recursive(int arr[], int low, int high, int x)
 int main()
 {
     int arr[] = {3,9,41,66,79};
-    int x=90;
+    int x=79;
     int idx = binary_recursive(arr, 0, 4, x);
     cout<<"The index of the element to be searched "<<x<<" is : "<<idx;
 }
---
+++ Searching/idx_first_occurence.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+int idx_first_occurence(int arr[], int , int );
+
+int idx_first_occurence_recursive(int arr[], int low, int high, int x)
+{
+    if(low>high)
+        return -1;
+
+    int mid=(low+high)/2;
+    
+    if(arr[mid]>x)
+    {
+        return idx_first_occurence_recursive(arr,low, mid-1, x);
+    }
+    else if(arr[mid]<x)
+    {
+        return idx_first_occurence_recursive(arr, mid+1, high, x);
+    }
+
+    else{
+        if(mid==0 || arr[mid-1]!=arr[mid])
+            return mid;
+        else
+            return idx_first_occurence_recursive(arr,low, mid-1, x);
+
+    }
+    
+}
+
+
+int idx_first_occurence(int arr[], int n, int x)
+{
+    int low=0, high=n-1; 
+    while(low<=high)
+    {
+        int mid=(low+high)/2;
+        if(arr[mid]>x)
+            high=mid-1;
+        else if(arr[mid]<x)
+            low=mid+1;
+        else{
+            if(mid==0 || arr[mid-1]!=arr[mid])
+                return mid;
+            else
+                high=mid-1;
+        }
+    }
+    return -1;
+}
+
+
+int main()
+{
+    int arr[] = {3,9,41,66,79};
+    int x=90;
+    int idx_r = idx_first_occurence_recursive(arr, 0, 4, x);
+    int idx = idx_first_occurence(arr, 0, 4, x);
+    cout<<"The index of the element to be searched "<<x<<" is : "<<idx_r;
+    cout<<"The index of the element to be searched "<<x<<" is : "<<idx;
+}
