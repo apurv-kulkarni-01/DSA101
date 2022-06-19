c29079eac4d28877940050f18575eee0fdcfdb49
Apurv <apurvkulkarni29@gmail.com>
2022-06-19 11:12:59 +0530

    Sorting

---
+++ Sorting/tempCodeRunnerFile.cpp
@@ -0,0 +1,16 @@
+cout<<"Sort in Array: "<<endl;
+    int arr[] = {9,21,47,4,5,87,11};
+    sort(arr, arr+7, greater<int>());
+    for(auto i : arr)
+    {
+        cout<<i<<" ";
+    }
+
+
+    cout<<endl<<endl<<"Sort in Vector: "<<endl;
+    vector<int> v={89,6,7,24,84,65,745,0};
+    sort(v.begin(), v.end());
+    for(auto i : v)
+    {
+        cout<<i<<" ";
+    }
