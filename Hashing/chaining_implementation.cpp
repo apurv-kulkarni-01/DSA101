c5cf47c75d957ea264b28336728272e72da66ff3
Apurv <apurvkulkarni29@gmail.com>
2022-07-14 11:12:26 +0530

    Hashing>chaining implementation

---
+++ Hashing/chaining_implementation.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#include<list>
+using namespace std;
+
+// struct Node{
+//     int data;
+//     Node* next;
+//     Node(int x){
+//         data = x;
+//         next=NULL;
+//     }
+// };
+
+// struct chain{
+//     Chain(int size){
+//         Node arr[size];f
+//     }
+// };
+
+struct MyHash{
+    int bucket;
+    list<int> *table;
+    MyHash(int b){
+        bucket = b;
+        table = new list<int>[b];
+    }
+    void insert(int key){
+        int idx = key%bucket;
+        table[idx].push_back(key);
+    }
+    bool search(int key){
+        int idx = key%bucket;
+        for(auto x: table[idx]){
+            if(x==key)
+                return true;
+        }
+        return false;
+    }
+    void remove(int key){
+        int idx = key%bucket;
+        table[idx].remove(key);
+    }
+};
+
+int main(){
+    MyHash m(7);
+    m.insert(70);
+    m.insert(71);
+    m.insert(9);
+    m.insert(56);
+    m.insert(72);
+    cout<<m.search(72);
+}
