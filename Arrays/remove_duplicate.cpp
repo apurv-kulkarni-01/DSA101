03c8f6590499614e3a21ae3c6b50a9a65162ec5a
Apurv <apurvkulkarni29@gmail.com>
2022-07-27 21:03:46 +0530

    remove duplicates from array

--- Arrays/remove_duplicate.cpp
+++ Arrays/remove_duplicate.cpp
@@ -1,14 +1,35 @@
 #include<iostream>
 using namespace std;
 
+// naive
+// T.C. = o(n)
+// A.S. = O(n)
+int remove_duplicate_naive(int arr[], int n)
+{
+    int temp[n];
+    temp[0]=arr[0];
+    int res = 1;
+    for(int i=1;i<n;i++){
+        if(temp[res-1]!=arr[i]){
+            temp[res]=arr[i];
+            res++;
+        }
+    }
+    for(int i=0;i<res;i++){
+        cout<<temp[i]<<" ";
+    }
+    return res;
+}
+
+// BEST
+// T.C. = o(n)
+// A.S. = O(1)
 int remove_duplicate(int arr[], int n)
 {
-    int res = 1;
-    for(int i=0;i<n;i++)
-    {
-        if(arr[res-1]!=arr[i])
-        {
-            arr[res] = arr[i];
+    int res=1;
+    for(int i=1;i<n;i++){
+        if(arr[res-1]!=arr[i]){
+            arr[res]=arr[i];
             res++;
         }
     }
@@ -21,7 +42,7 @@ int main()
     int arr[] ={5,5,3,3,1};
     int n=5;
     n = remove_duplicate(arr, n);
-    cout<<"The size of non duplicate array: "<<n<<endl;
+    cout<<endl<<"The size of non duplicate array: "<<n<<endl;
 
     for(int i=0;i<n;i++)
     {
