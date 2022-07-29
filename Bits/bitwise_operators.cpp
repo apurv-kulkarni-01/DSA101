9c208d041a7dda162fe94bf3872892969f98eeb2
Apurv <apurvkulkarni29@gmail.com>
2022-07-29 23:28:14 +0530

    bitwise operators

---
+++ Bits/bitwise_operators.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x=3, y=6;
+    cout<<"bitwise AND: \n";
+    cout<<(x&y);
+    cout<<"\nbitwise OR: \n";
+    cout<<(x|y);
+    cout<<"\nbitwise XOR: \n";
+    cout<<(x^y);
+    cout<<"\nLeftshift operator: \n";
+    cout<<(x<<2);
+    cout<<"\nRightshift operator: \n";
+    cout<<(33>>2);
+}
