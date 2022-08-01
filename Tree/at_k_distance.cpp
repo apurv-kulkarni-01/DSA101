8d37e6d1978d82fcf6c4d477cc11605d61f91274
Apurv <apurvkulkarni29@gmail.com>
2022-08-01 22:21:03 +0530

    Nodes at k distance

---
+++ Tree/at_k_distance.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+    Node(int x){
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void atK(Node* root, int k){
+    if(root==NULL){return;}
+    // if(k==0){
+    //     cout<<root->data<<" ";
+    // }
+    if(root!=NULL){
+        if(k!=0){
+            atK(root->left, k-1);
+            atK(root->right, k-1);
+        }
+        if(k==0){
+            cout<<root->data<<" ";
+        }
+    }
+}
+
+int main(){
+    // Node* root = new Node(10);
+    // root->left = new Node(20);
+    // root->left->left = new Node(30);
+
+    Node* root = new Node(10);
+    root->left = new Node(20);
+    root->right = new Node(30);
+    root->right->right = new Node(70);
+    root->right->right->right = new Node(80);
+    root->left->left = new Node(40);
+    root->left->right = new Node(50);
+
+    atK(root,2);
+}
+
